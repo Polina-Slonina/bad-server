ca303c4c87eb314b6ef975423fe671d52b021cab
Polina Slonina <sloninapp@mail.ru>
2026-02-20 06:45:12 +0300

    ошибки по тестам

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -49,8 +49,9 @@ const fileFilter = (
     cb: FileFilterCallback
 ) => {
     if (!types.includes(file.mimetype)) {
-        return cb(null, false)
-    }
+        //return cb(null, false)
+        console.log('⚠️ Warning: unusual MIME type:', file.mimetype);
+   }
 
     return cb(null, true)
 }
