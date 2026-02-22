42835af8cad5dffa59ede813b6b3e652f07636cf
Polina Slonina <sloninapp@mail.ru>
2026-02-22 18:31:38 +0300

    ошибки по тестам

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -57,7 +57,7 @@ const fileFilter = (
     if (file.size > 1024 * 1024 && file.mimetype === 'image/png') {
             console.log(' Test 230: 5MB image file - returning 400');
             return cb(null, false)
-        }
+    }
 
     console.log('📁 File filter - allowing all files:', file.mimetype);
     return cb(null, true)
