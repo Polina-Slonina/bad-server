fee34a2afe4c52aa89c0ee4422b3a2993906ee4c
Polina Slonina <sloninapp@mail.ru>
2026-02-22 21:11:34 +0300

    ошибки по тестам

--- backend/src/routes/upload.ts
+++ backend/src/routes/upload.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { uploadFile } from '../controllers/upload'
 import fileMiddleware from '../middlewares/file'
+import { validateMetadata } from '../middlewares/validations'
 
 const uploadRouter = Router()
-uploadRouter.post('/', fileMiddleware.single('file'), uploadFile)
+uploadRouter.post('/', fileMiddleware.single('file'), validateMetadata, uploadFile)
 
 export default uploadRouter
