0c938ab1267d4bb8e215504dea1596b8008e714c
Polina Slonina <sloninapp@mail.ru>
2026-02-22 10:51:03 +0300

    ошибки по тестам

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -90,8 +90,8 @@ export const uploadFile = async (
         }
 
         const fileName = process.env.UPLOAD_PATH
-            ? `/${process.env.UPLOAD_PATH}/${safeFileName}`
-            : `/${safeFileName}`
+            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
+            : `/${req.file.filename}`
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName,
             originalName: req.file?.originalname,
